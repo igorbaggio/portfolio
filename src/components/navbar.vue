cdc2f075c213d450de95c281a72d54f2b3e86bdb
IgorBaggio <baggiosigor@gmail.com>
2023-08-11 20:59:18 -0300

    Navbar com nome, contato/sobre

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="navbar">Meu navbar</div>
+  <div class="navbar">
+    <div class="navbar__left">Igor Baggio</div>
+    <div class="navbar__right">
+      <div class="navbar__right--sobre">Sobre | </div>
+      <div class="navbar__right--contato">Contato</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +13,19 @@ export default {
   name: "myNavbar",
 };
 </script>
-<style scoped></style>
+<style scoped>
+.navbar {
+  height: 80px;
+  background-color: lightgray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.navbar__right{
+  display: flex;
+  justify-content: right;
+}
+.navbar__right--contato{
+  padding-left: 4px;
+}
+</style>
