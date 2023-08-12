aad0dc65be5242759ab2b8bbd095d2092373fed6
IgorBaggio <baggiosigor@gmail.com>
2023-08-11 21:47:04 -0300

    Footer, v.1 / 2023

---
+++ src/components/footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+      <div class="footer__description">V.1 2023</div>
+      
+      </div>
+
+    
+  </template>
+  
+  <script>
+  export default {
+    name: "myFooter",
+  };
+  </script>
+  <style scoped>
+  .footer {
+    height: 80px;
+    background-color: lightgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    bottom: 0px;
+    position: fixed;
+    width: 100%;
+    
+  }
+  .footer__description{
+
+  }
+ 
+ 
+  </style>
+  
